8f0c48af0bf89e2284ba661f4b77b6d5ba5b9b4e
Chris Rasmussen <chris@digitalformula.net>
2019-06-24 17:55:21 +1000

    Add get cluster info script

---
+++ python/get_cluster_info_v3/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+with open('readme.rst', encoding='UTF-8') as f:
+    readme = f.read()
+
+setup(
+    name='nutanix-cluster-info',
+    version='3.1',
+    description='Use the Prism Central API to get Nutanix environment info.',
+    long_description=readme,
+    author='Chris Rasmussen',
+    author_email='crasmussen@nutanix.com',
+    install_requires=[
+        'requests',
+        'urllib3',
+        'weasyprint'
+    ],
+    packages=find_packages('.'),
+    package_dir={'': '.'}
+)
